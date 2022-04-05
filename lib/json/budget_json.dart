0d289ff5af6afaa26ef40023585d131faf674b23
Supreeti Kumari <78208708+SupreetiKumari@users.noreply.github.com>
2022-04-05 23:57:50 +0530

    Delete lib directory

--- lib/json/budget_json.dart
+++
@@ -1,9 +0,0 @@
-import 'package:budgetapp/common/color_constants.dart';
-
-const List budget_json = [
-  {"name": "Gift", "price": "\$400.00", "color": green},
-  {"name": "market", "price": "\$400.00", "color": green},
-  {"name": "school fees", "price": "\$400.00", "color": green},
-  {"name": "vehicle", "price": "\$40.00", "color": green},
-  {"name": "banking", "price": "\$4000.00", "color": green}
-];
--- lib/screens/categories.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Category{
-
-  String name;
-  Color clr;
-  Widget icon;
-  Category(String categoryName, Color color, Widget categoryIcon) : name = categoryName, clr = color, icon = categoryIcon;
-}
